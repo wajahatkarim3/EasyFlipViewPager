6c67b7670a92539238d8018bf8b5b9fe792608e2
Wajahat Karim <wajahatkarim3@gmail.com>
2018-12-02 20:35:37 +0500

    Adds showcase demo. Fixes issue #4

--- app/src/main/java/com/wajahatkarim3/easyflipviewpager/demo/GalleryImageFragment.kt
+++ app/src/main/java/com/wajahatkarim3/easyflipviewpager/demo/GalleryImageFragment.kt
@@ -1,17 +1,19 @@
 package com.wajahatkarim3.easyflipviewpager.demo
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
 class GalleryImageFragment : Fragment() {
 
     internal var title = ""
-    internal var description = ""
+    internal var description: String? = null
     internal var imageId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,32 +22,55 @@ class GalleryImageFragment : Fragment() {
         arguments?.let {
             imageId = it.getInt("imageId", R.drawable.ic_launcher_background)
             title = it.getString("title", "")
-            description = it.getString("description", "")
+            description = it.getString("description", null)
         }
 
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment_gallery_page_layout, container, false)
 
-        val imageView = rootView.findViewById<ImageView>(R.id.imageView)
-        val txtTitle = rootView.findViewById<TextView>(R.id.txtTitle)
-        val txtDescription = rootView.findViewById<TextView>(R.id.txtDescription)
+        if(description != null)
+        {
+            val rootView = inflater.inflate(R.layout.fragment_gallery_page_layout, container, false)
 
-        txtTitle.text = title
-        txtDescription.text = description
-        imageView.setImageResource(imageId)
+            val imageView = rootView.findViewById<ImageView>(R.id.imageView)
+            val txtTitle = rootView.findViewById<TextView>(R.id.txtTitle)
+            val txtDescription = rootView.findViewById<TextView>(R.id.txtDescription)
 
-        return rootView
+            txtTitle.text = title
+            txtDescription.text = description
+            imageView.setImageResource(imageId)
+            return rootView
+        }
+        else {
+            val rootView = inflater.inflate(R.layout.fragment_demo_layout, container, false)
+
+            val imageView = rootView.findViewById<ImageView>(R.id.imageView)
+            val txtTitle = rootView.findViewById<TextView>(R.id.txtTitle)
+            val btnLaunch = rootView.findViewById<Button>(R.id.btnLaunch)
+
+            txtTitle.text = title
+            imageView.setImageResource(imageId)
+            btnLaunch.setOnClickListener {
+                when(imageId)
+                {
+                    R.drawable.books_snap -> startActivity(Intent(context, BookOnboardingActivity::class.java))
+                    R.drawable.gallery_snap -> startActivity(Intent(context, PictureGalleryDemoActivity::class.java))
+                    R.drawable.poker_snap -> startActivity(Intent(context, PokerCardDemoActivity::class.java))
+                }
+            }
+
+            return rootView
+        }
     }
 
     companion object {
-        fun newInstance(title: String, subtitle: String, imageId: Int): GalleryImageFragment
+        fun newInstance(title: String, subtitle: String?, imageId: Int): GalleryImageFragment
         {
             val fragment = GalleryImageFragment()
             val args = Bundle()
             args.putString("title", title)
-            args.putString("description", subtitle)
+            subtitle?.let { args.putString("description", subtitle) }
             args.putInt("imageId", imageId)
             fragment.arguments = args
             return fragment
--- app/src/main/java/com/wajahatkarim3/easyflipviewpager/demo/MainActivity.kt
+++ app/src/main/java/com/wajahatkarim3/easyflipviewpager/demo/MainActivity.kt
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
+import android.widget.CheckBox
+import android.widget.RadioGroup
 import com.wajahatkarim3.easyflipviewpager.BookFlipPageTransformer
 import com.wajahatkarim3.easyflipviewpager.CardFlipPageTransformer
 
@@ -15,19 +18,87 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mPager: ViewPager
     private var mPagerAdapter: PagerAdapter? = null
+    lateinit var radioGroupFlipAnimation: RadioGroup
+    lateinit var checkEnableScale: CheckBox
+
+    var bookFlipTransformer = BookFlipPageTransformer()
+    var cardFlipTransformer = CardFlipPageTransformer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        radioGroupFlipAnimation = findViewById(R.id.rgFlipAnimation)
+        checkEnableScale = findViewById(R.id.checkEnableScale)
+
+        // Book Flip Transformer
+        bookFlipTransformer.isEnableScale = true
+        bookFlipTransformer.scaleAmountPercent = 10f
+
+        // Card Flip Transformer
+        cardFlipTransformer.isScalable = false
+        cardFlipTransformer.flipOrientation = CardFlipPageTransformer.VERTICAL
+
         // Instantiate a ViewPager and a PagerAdapter.
         mPager = findViewById<ViewPager>(R.id.pager)
-        mPagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        mPagerAdapter = DemoPagerAdapter(supportFragmentManager)
         mPager.adapter = mPagerAdapter
         mPager.clipToPadding = false
-        var transformer = CardFlipPageTransformer()
-        transformer.flipOrientation = CardFlipPageTransformer.VERTICAL
-        transformer.isScalable = false
-        mPager.setPageTransformer(true, transformer)
+
+        updatePagerConfigs()
+
+        radioGroupFlipAnimation.setOnCheckedChangeListener { radioGroup, id ->
+            updatePagerConfigs()
+        }
+
+        checkEnableScale.setOnCheckedChangeListener { compoundButton, value ->
+            updatePagerConfigs()
+        }
+    }
+
+    fun updatePagerConfigs()
+    {
+        when(radioGroupFlipAnimation.checkedRadioButtonId)
+        {
+            R.id.radioBookFlip -> {
+                bookFlipTransformer.isEnableScale = checkEnableScale.isChecked
+                mPager.setPageTransformer(true, bookFlipTransformer)
+            }
+            R.id.radioCardFlip -> {
+                cardFlipTransformer.isScalable = checkEnableScale.isChecked
+                mPager.setPageTransformer(true, cardFlipTransformer)
+            }
+        }
+    }
+
+    class DemoPagerAdapter : FragmentPagerAdapter
+    {
+        var fragmentsList = arrayListOf<GalleryImageFragment>()
+
+        constructor(fm: FragmentManager) : super(fm)
+        {
+            val titles = arrayOf(
+                    "Book Onboarding",
+                    "Poker Card",
+                    "Pakistan Gallery"
+            )
+
+            val imageIds = intArrayOf(
+                    R.drawable.books_snap,
+                    R.drawable.poker_snap,
+                    R.drawable.gallery_snap
+            )
+
+            for (i in 0 until imageIds.size)
+            {
+                var frag = GalleryImageFragment.newInstance(titles[i], null, imageIds[i])
+                fragmentsList.add(frag)
+            }
+        }
+
+        override fun getItem(position: Int): Fragment = fragmentsList[position]
+
+        override fun getCount() = fragmentsList.size
+
     }
 }
